b3eb8f28b46d6e98502354e8f01e8c610eb569f2
sweun2 <sweun3@gmail.com>
2022-08-07 00:13:54 +0900

    fix: Add oauth config

--- src/main/java/dcom/refrigerator/api/global/security/config/SecurityConfig.java
+++ src/main/java/dcom/refrigerator/api/global/security/config/SecurityConfig.java
@@ -1,17 +1,31 @@
 package dcom.refrigerator.api.global.security.config;
 
 
+import dcom.refrigerator.api.global.oauth2.CustomOAuth2UserService;
+import dcom.refrigerator.api.global.oauth2.OAuth2SuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
-
+    private final CustomOAuth2UserService oAuth2UserService;
+    private final OAuth2SuccessHandler successHandler;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeRequests().antMatchers("/test").permitAll().anyRequest().authenticated();
+        http.csrf().disable().httpBasic().disable()
+                .authorizeRequests().antMatchers("/test").permitAll().anyRequest().authenticated()
+
+                .and()
+                .oauth2Login()
+                .successHandler(successHandler)
+                .userInfoEndpoint().userService(oAuth2UserService);
+
 
         return http.build();
     }
