d0f37fee95e07080d483f596910c4a54518c4a72
sweun2 <sweun3@gmail.com>
2022-08-07 05:02:15 +0900

    fix: antMatch("/test") -> anyRequest()

--- src/main/java/dcom/refrigerator/api/global/security/config/SecurityConfig.java
+++ src/main/java/dcom/refrigerator/api/global/security/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,8 +19,9 @@ public class SecurityConfig {
     private final OAuth2SuccessHandler successHandler;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf().disable().httpBasic().disable()
-                .authorizeRequests().antMatchers("/test").permitAll().anyRequest().authenticated()
+        http.csrf().disable().httpBasic().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests().anyRequest().permitAll()
 
                 .and()
                 .oauth2Login()
