aab8c6eda48be523031878be550d59509f8a0fdc
sweun2 <sweun3@gmail.com>
2022-08-07 00:15:58 +0900

    feat: Add method findById

--- src/main/java/dcom/refrigerator/api/domain/user/repository/UserRepository.java
+++ src/main/java/dcom/refrigerator/api/domain/user/repository/UserRepository.java
@@ -2,6 +2,11 @@ package dcom.refrigerator.api.domain.user.repository;
 
 import dcom.refrigerator.api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findById(Integer id);
 }
