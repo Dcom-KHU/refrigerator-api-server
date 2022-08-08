35efccd4381221b877da0aa4a6a5d7f16dc8d0e2
sweun2 <sweun3@gmail.com>
2022-08-08 17:55:34 +0900

    fixchange sessionPolicy -> unused,

--- src/main/java/dcom/refrigerator/api/global/security/config/SecurityConfig.java
+++ src/main/java/dcom/refrigerator/api/global/security/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,17 +18,24 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     private final CustomOAuth2UserService oAuth2UserService;
     private final OAuth2SuccessHandler successHandler;
+    private final TokenService tokenService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf().disable().httpBasic().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests().anyRequest().permitAll()
+        http.httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
                 .and()
-                .oauth2Login()
+                .authorizeRequests()
+                .antMatchers("/token/**").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .oauth2Login().loginPage("/token/expired")
                 .successHandler(successHandler)
                 .userInfoEndpoint().userService(oAuth2UserService);
 
+        http.addFilterBefore(new JwtAuthFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
