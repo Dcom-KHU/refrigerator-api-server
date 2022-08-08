b002fde9ce8ae33fd9c6f17d59fdb9548773b44b
sweun2 <sweun3@gmail.com>
2022-08-08 17:53:46 +0900

    feat: Add User join logic

--- src/main/java/dcom/refrigerator/api/global/oauth2/OAuth2SuccessHandler.java
+++ src/main/java/dcom/refrigerator/api/global/oauth2/OAuth2SuccessHandler.java
@@ -7,11 +7,16 @@ import dcom.refrigerator.api.global.security.config.Token;
 import dcom.refrigerator.api.global.security.config.TokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.persistence.Column;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,7 +26,9 @@ import java.util.Optional;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+
     private final TokenService tokenService;
     private final UserRepository userRepository;
     private final ObjectMapper objectMapper;
@@ -29,17 +36,60 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
+
+
+
+
         OAuth2User oAuth2User = (OAuth2User)authentication.getPrincipal();
-        Integer uid = (Integer) oAuth2User.getAttribute("id");
-        Optional<User> userOptional = userRepository.findById(uid);
 
-        // 최초 로그인이라면 회원가입 처리를 한다.
+        String email = (String) oAuth2User.getAttribute("email");
 
-        Token token = tokenService.generateToken(Integer.toString(uid), "USER");
-        log.info("{}", token);
 
+        Optional<User> userOptional = userRepository.findByEmail(email);
+
+
+        log.info("{}", userOptional);
+
+
+        if( userOptional.isPresent()){
+            User user=userOptional.get();
+        }
+        else {
+            User user = userRepository.save(
+                    User.builder()
+                            .nickname((String) oAuth2User.getAttribute("nickname"))
+                            .email(email)
+                            .name((String) oAuth2User.getAttribute("name"))
+                            .notificationRefrigerator(false)
+                            .notificationFood(false)
+                            .build()
+            );
+        }
+
+        Token token = tokenService.generateToken(email, "USER");
+        /*ResponseCookie tokenCookie = ResponseCookie.from("accessToken", token.getToken())
+                .maxAge(60*10)
+                .path("/")
+                .httpOnly(true)
+                .sameSite("none")
+                .build();
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", token.getRefreshToken())
+                .maxAge(60*60*24*7*2)
+                .path("/")
+                .httpOnly(true)
+                .sameSite("none")
+                .build();
+
+
+
+        response.addHeader("Set-Cookie",tokenCookie.toString());
+        response.addHeader("Set-Cookie",refreshTokenCookie.toString());
+
+    */
+        //getRedirectStrategy().sendRedirect(request, response,'' );
         writeTokenResponse(response, token);
     }
+
 
     private void writeTokenResponse(HttpServletResponse response, Token token)
             throws IOException {
@@ -53,4 +103,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         writer.println(objectMapper.writeValueAsString(token));
         writer.flush();
     }
+
+
+
 }
