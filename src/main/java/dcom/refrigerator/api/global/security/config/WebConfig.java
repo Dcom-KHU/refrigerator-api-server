fc5626b9be1bd3597513a2d45fa921f53b41d758
sweun2 <sweun3@gmail.com>
2022-08-11 22:43:40 +0900

    feat: Add Cors config

---
+++ src/main/java/dcom/refrigerator/api/global/security/config/WebConfig.java
@@ -0,0 +1,13 @@
+package dcom.refrigerator.api.global.security.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
